aace73688d337f9146e5f820cd65785ddd5c1640
vskolotov <vskolotov@ya.ru>
2022-02-16 23:45:32 +0500

    creating a model. Migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    ADMINISTRATOR = 'admin'
+    MANAGER = 'manager'
+    DEVELOPER = 'dev'
+    USER = 'user'
+
+    ROLE_CHOICES = (
+        (ADMINISTRATOR, 'Администратор'),
+        (MANAGER, 'Менеджер проекта'),
+        (DEVELOPER, 'Разрабочик'),
+        (USER, 'Пользователь')
+    )
+    email = models.EmailField(verbose_name='email', unique=True, blank=False)
+    first_name = models.CharField(verbose_name='name', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='surname', max_length=30, blank=True)
+    date_joined = models.DateTimeField(verbose_name='registered', auto_now_add=True)
+    rule = models.CharField(verbose_name='rule', max_length=64, choices=ROLE_CHOICES, default=USER)
