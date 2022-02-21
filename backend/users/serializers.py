7b013b34c7929604574b66fec62fe9983dc108b8
vskolotov <vskolotov@ya.ru>
2022-02-21 23:39:56 +0500

    new project

--- backend/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import SiteUser
-
-
-class SiteUserModelSerializer(ModelSerializer):
-    class Meta:
-        model = SiteUser
-        fields = '__all__'
--- backend/users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .serializers import SiteUserModelSerializer
-from .models import SiteUser
-
-
-class SiteUserViewSet(ModelViewSet):
-    serializer_class = SiteUserModelSerializer
-    queryset = SiteUser.objects.all()
