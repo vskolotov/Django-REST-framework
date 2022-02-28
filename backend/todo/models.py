ae985174974e777cfe9e593a60ec23248bc64754
vskolotov <vskolotov@ya.ru>
2022-02-28 21:59:42 +0500

    Project and Note model consciousness

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from backend.users.models import SiteUser
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(verbose_name='title', max_length=64, blank=False)
+    repository = models.URLField(verbose_name='repository', max_length=128, blank=True)
+    users = models.ManyToManyField(SiteUser)
+
+
+class Note(models.Model):
+    text = models.TextField(max_length=1024)
+    created = models.DateTimeField(verbose_name='created', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='updated', auto_now=True)
+    is_active = models.BooleanField(verbose_name='active', default=True)
+    user = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
+    project = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
